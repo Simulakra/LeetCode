68b099a116b3d3e1c8bcc2bf285f34dd065ecff5
Simulakra <tr.berkedundar@gmail.com>
2022-11-27 03:10:52 +0300

    9-PalindromeNumber

---
+++ 9-PalindromeNumber/main.py
@@ -0,0 +1,18 @@
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    y=str(x)
+    for i in range(int(len(y)/2)):
+      if(i==0):
+        if( y[i] != y[-(i+1):] ):
+          return False
+      else:
+        if( y[i] != y[-(i+1):-i] ):
+          return False
+    return True
+
+print(Solution.isPalindrome(Solution, 12121))
+print(Solution.isPalindrome(Solution, 12345))
+print(Solution.isPalindrome(Solution, 1234))
+print(Solution.isPalindrome(Solution, -12121))
+print(Solution.isPalindrome(Solution, 1111))
+print(Solution.isPalindrome(Solution, 11111))
